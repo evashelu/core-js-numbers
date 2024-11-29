999431788d49735099f2b5485c5ca9d6ce42c6b7
evashelu <evavolk43@gmail.com>
2024-11-29 11:41:06 +0300

    fix: fixed func isSaveInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -402,8 +402,8 @@ function toPrecision(number, precision) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(input) {
+  return Number(input);
 }
 
 /**
@@ -483,8 +483,8 @@ function getIntegerOnString(/* str, base */) {
  * 3.5      => false
  * 2 ** 53  => false
  */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+function isSafeInteger(value) {
+  return Number.isSafeInteger(value);
 }
 
 /**
